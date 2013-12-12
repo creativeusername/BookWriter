1a879885bb034dbada0f4b8a5ad8247bc9908f11
Maximilian Kerp <mkerp2s@smail.inf.h-brs.de>
2013-12-12 23:31:36 +0100

    Seeds.rb eingefuegt

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+felix = User.create!(email: "fthiel2s@smail.inf.h-brs.de", first_name: "Felix", last_name: "Thiele", password: "12345678", password_confirmation: "12345678", username: "fthiel2s")
+max = User.create!(email: "mkerp2s@smail.inf.h-brs.de", first_name: "Max", last_name: "Kerp", password: "12345678", password_confirmation: "12345678", username: "mkerp2s")
+jan_eric = User.create!(email: "emueller2s@smail.inf.h-brs.de", first_name: "Jan Eric", last_name: "Mueller", password: "12345678", password_confirmation: "12345678", username: "emueller2s")
